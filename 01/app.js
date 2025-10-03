b02d081df6b00a47c6b5a1a217fe180a67fab487
Michal Wasiak <michal.wasiak@hotmail.com>
2025-10-03 14:28:06 +1000

    Tasko 01 done

--- 01/app.js
+++ 01/app.js
@@ -2,15 +2,22 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
+    setBorderColorAsync(divList[0], 'red', secondDiv)
+    function secondDiv() {
+        setBorderColorAsync(divList[1], 'blue', thirdDiv)
+    }
+    function thirdDiv() {
+        setBorderColorAsync(divList[2], 'green', () => { })
+    }
 
+    // setBorderColorAsync(divList[0], 'red', function () {
+    //     setBorderColorAsync(divList[1], 'blue', function () {
+    //         setBorderColorAsync(divList[2], 'green', function () {
+    //             console.log('finish');
+    //         });
+    //     });
+    // });
+    // secondDiv();
 }
 
 function setBorderColorAsync(element, color, callback) {
