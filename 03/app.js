9d978e4dd6359c7c3e70c3823020c803bfc0412d
Michal Wasiak <michal.wasiak@hotmail.com>
2025-10-03 14:42:55 +1000

    Tasko 03 done

--- 03/app.js
+++ 03/app.js
@@ -3,3 +3,36 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     console.log('DOM');
 }
+
+const btnEl = document.querySelector('button')
+btnEl.addEventListener('click', handleBtn)
+// btnEl.addEventListener('click', insertIP)
+
+function handleBtn() {
+    const promise = fetch('https://api.ipify.org?format=json');
+    promise
+        .then(resp => {
+            if (resp.ok) { return resp.json(); }
+            return Promise.reject(resp);
+        })
+        .then(resp => {
+            console.log(resp)
+            const spanEl = document.querySelector('span')
+            if (spanEl) {
+                spanEl.innerText = resp.ip;
+                console.log(spanEl)
+            }
+        })
+        .catch(err => console.error(err))
+        .finally(() => {
+            console.log('Odpytywanie API zakończone!')
+        });
+}
+
+// function insertIP(e) {
+//     const spanEl = document.querySelector('span')
+//     if (spanEl) {
+//         spanEl.innerText = e.target.ip;
+//         console.log(spanEl)
+//     }
+// }
