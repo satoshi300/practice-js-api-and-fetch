a5d8b623ca22c611c4be309b93321ce54a177a55
Michal Wasiak <michal.wasiak@hotmail.com>
2025-10-06 15:19:12 +1000

    Task 05 - remove comments

--- 05/app.js
+++ 05/app.js
@@ -1,18 +1,3 @@
-// W tym zadaniu będziesz potrzebować narzędzia [JSON Server](https://github.com/typicode/json-server), które uruchomi lokalne API na podstawie pliku `data.json`. JSON Server instalowaliśmy globalnie podczas przerabiania materiałów z tego modułu.
-
-// W pliku tym znajdują się już dane, które zostaną załadowane do HTML-a za pomocą kodu napisanego w `app.js` zaraz po uruchomieniu lokalnego API.
-
-// JSON Server można uruchomić w terminalu przy pomocy komendy: `json-server ./data.json --watch`.
-
-// Twoim zadaniem jest napisanie obsługi formularza, tak aby można było dodawać nowe dane do naszego lokalnego API.
-
-// Przypominam, że należy:
-// * wykorzystać odpowiednią metodę (`POST`),
-// * na podstawie wysłanych przez formularz danych utworzyć obiekt, który trzeba zamienić na format JSON (`JSON.stringify()`),
-// * przekazanć odpowiedni nagłówek (`'Content-Type': 'application/json'`).
-
-// Po każdorazowym dodaniu użytkownika zaktualizuj widok przy pomocy funkcji `loadUsers()`. Trzeba ją uruchomić w odpowiednim momencie, np. w `finally()`.
-
 const apiUrl = 'http://localhost:3000/users';
 console.log(apiUrl)
 
@@ -51,6 +36,7 @@ function addUser() {
     form.addEventListener('submit', e => {
         e.preventDefault();
         data = { firstName: firstName.value, lastName: lastName.value }
+
         const options = {
             method: 'POST',
             body: JSON.stringify(data),
