97122228e256ac0ba8a1de177b4560dcacca3775
Michal Wasiak <michal.wasiak@hotmail.com>
2025-10-06 12:01:05 +1000

    Task 02 - remove comments

--- 02/app.js
+++ 02/app.js
@@ -1,12 +1,3 @@
-// W pliku `app.js` ponownie masz przygotowany kod, który ustawia kolor obramowania dla wyszukanych `divów`. Tym razem jest on zabezpieczony przed podaniem nieprawidłowego elementu czy przed brakiem *callbacka*.
-
-// Twoim zadaniem jest przebudowanie funkcji `setBorderColorAsync()` w taki sposób, aby realizowała swoje dotychczasowe zadanie przy pomocy obietnic (`Promise`), tj. powinna zwracać obiekt utworzony przy pomocy `new Promise( (resolve, reject) => ...)`;
-
-// Pamiętaj, że `resolve` to funkcja, która jest uruchamiana w przypadku powodzenia, natomiast `reject` – w przypadku, gdy coś jest nie tak.
-
-// Po wprowadzeniu zmian musisz również zmienić wykorzystanie tej funkcji zgodnie z jej obecną implementacją. Pamiętaj, że `.then` może być wykorzystane wielokrotnie.
-
-
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
